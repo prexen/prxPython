b07322d357a206d5889a57bef949a86ff38fafd1
prexen <prexen@users.noreply.github.com>
2019-04-18 10:04:59 -0300

    Update ListaLoop.py

--- ListaLoop.py
+++ ListaLoop.py
@@ -16,3 +16,18 @@ def main():
 
 
 main();
+
+
+#pede inputs e mostra o menor deles ate ambos os numero serem iguais.
+def questao1():
+    programRunning = True;
+    while(programRunning):
+        num1 = float (input("first real number!..."));
+        num2 = float (input("second real number..."));
+        if(num1 < num2):
+            print("menor numero é: " , num1)
+        elif(num1 > num2):
+            print("menor numero é: " , num2)
+        elif(num1 == num2):
+            print("end program.")
+            programRunning = False;
